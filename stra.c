53294d5fd4cd992f77c05675be11195a6c4ff795
haamizhk <hh2917@princeton.edu>
2026-02-27 02:19:06 -0500

    back to normal

--- stra.c
+++ stra.c
@@ -23,7 +23,7 @@ char *Str_copy(char dst[], const char src[])
         index++;
     }
     dst[index] = '\0';
-    return dst + 1;
+    return dst;
 }
 
 char *Str_concat(char dst[], const char src[])
